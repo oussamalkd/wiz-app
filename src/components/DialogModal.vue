ec13b2629108f2a943b0cd378cffb1c3574349cc
Oussama Louelkadi <oussamalkd@pop-os.localdomain>
2022-09-01 19:32:58 +0100

    feat: cards page

---
+++ src/components/DialogModal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="modal-container">
+        <input type="checkbox" id="my-modal" class="modal-toggle" />
+        <div class="modal">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Are You Sure?</h3>
+            <p class="py-4">Do you really want to delete this user? This process canoot be undone.</p>
+            <div class="modal-action">
+                <label for="my-modal" class="btn btn-ghost">Cancel</label>
+                <!-- check the user if click delete button and send the value to parent-->
+                <label for="my-modal" class="btn btn-error" @click="$emit('confirmed', true)">Delete</label>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'DialogModal',
+}
+</script>
---
+++ src/components/IndexCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="index-card">
+        <div class="card-wrapper">
+            <div class="card-header">
+                {{ title }}
+            </div>
+            <div class="card-content">
+                {{ subTitle }}
+            </div>
+            <!-- The button to open dialog modal -->
+            <!-- Send the id of selected user to parent  -->
+            <label for="my-modal" class="btn btn-link modal-button" @click="$emit('getCurrentUser',id)">Delete</label>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "IndexCard",
+    props: {
+        title: {
+            default: 'Card Title',
+        },
+        subTitle: {
+            default: 'Subtitle content'
+        },
+        id: {
+            require: true
+        }
+    }
+}
+</script>
+<style lang="scss">
+.index-card {
+    @apply bg-neutral-content shadow-lg flex flex-col rounded-lg w-full xl:w-5/12 2xl:w-3/12 m-5;
+    transition: all 0.5s ease-in-out;
+    cursor: pointer;
+
+    .card-wrapper {
+        @apply p-4 text-left;
+
+        .card-header {
+            @apply text-xl font-medium text-gray-900
+        }
+        .card-content {
+            @apply text-gray-400 font-medium text-lg
+
+        }
+    }
+
+    &:hover {
+        @apply bg-gray-50
+    }
+}
+</style>
